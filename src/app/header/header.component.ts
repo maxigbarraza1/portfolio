1a66d576b52e1b372a1fd57b0533d70728c2597d
Maxi Barraza <barrazagabrielm@gmail.com>
2023-12-27 20:44:40 -0300

    Add header component with navigation links

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  constructor(private _activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    console.log(this._activatedRoute.snapshot.url[0].path);
+  }
+}
