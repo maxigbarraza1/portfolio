4ea4998818725c0e6e206005f815c08d79399a1f
Maxi Barraza <barrazagabrielm@gmail.com>
2023-12-28 11:40:39 -0300

    Add section header component

---
+++ src/app/components/section-header/section-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-section-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-header.component.html',
+  styleUrl: './section-header.component.scss',
+})
+export class SectionHeaderComponent {
+  @Input() title: string = '';
+}
